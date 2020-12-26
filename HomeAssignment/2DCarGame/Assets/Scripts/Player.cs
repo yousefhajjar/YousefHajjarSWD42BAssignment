f3db97004d6c33a326d8723a72a36db6f059c73d
yousefhajjar <73161769+yousefhajjar@users.noreply.github.com>
2020-12-27 00:38:55 +0100

    move(), backgroundScroller & canvas
    added movement to the playerCar (left and right).
    added background Scrolling so that it appears that the car is moving forward
    added a few more assets

---
+++ HomeAssignment/2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] float moveSpeed = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+
+        transform.position = new Vector2(newXPos, -6.5f);
+    }
+}
